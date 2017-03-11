685c6b87dd9eb89a29ad8602a892f56ff3d58c92
alastairknowles <alastair@prism.hr>
2017-03-11 16:18:54 +0000

    Flyway problem with formatting.

--- src/main/resources/db/migration/V1.0__initial_schema.sql
+++ src/main/resources/db/migration/V1.0__initial_schema.sql
@@ -1,65 +1,65 @@
-create table user (
-  id                bigint auto_increment,
-  given_name        varchar(30)  not null,
-  surname           varchar(40)  not null,
-  email             varchar(254) not null,
-  stormpath_id      varchar(30)  not null,
-  created_timestamp datetime     not null,
-  updated_timestamp datetime,
-  primary key (id),
-  unique index (email),
-  unique index (stormpath_id),
-  index (created_timestamp),
-  index (updated_timestamp)
+CREATE TABLE user (
+  id                BIGINT AUTO_INCREMENT,
+  given_name        VARCHAR(30)  NOT NULL,
+  surname           VARCHAR(40)  NOT NULL,
+  email             VARCHAR(254) NOT NULL,
+  stormpath_id       VARCHAR(30)  NOT NULL,
+  created_timestamp DATETIME     NOT NULL,
+  updated_timestamp DATETIME,
+  PRIMARY KEY (id),
+  UNIQUE INDEX (email),
+  UNIQUE INDEX (stormpath_id),
+  INDEX (created_timestamp),
+  INDEX (updated_timestamp)
 )
-  collate = utf8_general_ci
-  engine = innodb;
+  COLLATE = utf8_general_ci
+  ENGINE = innodb;
 
-create table document (
-  id                bigint auto_increment,
-  cloudinary_id     varchar(30)  not null,
-  cloudinary_url    varchar(255) not null,
-  file_name         varchar(500) not null,
-  created_timestamp datetime     not null,
-  updated_timestamp datetime,
-  primary key (id),
-  index (created_timestamp),
-  index (updated_timestamp)
+CREATE TABLE document (
+  id                BIGINT AUTO_INCREMENT,
+  cloudinary_id     VARCHAR(30)  NOT NULL,
+  cloudinary_url    VARCHAR(255) NOT NULL,
+  file_name         VARCHAR(500) NOT NULL,
+  created_timestamp DATETIME     NOT NULL,
+  updated_timestamp DATETIME,
+  PRIMARY KEY (id),
+  INDEX (created_timestamp),
+  INDEX (updated_timestamp)
 )
-  collate = utf8_general_ci
-  engine = innodb;
+  COLLATE = utf8_general_ci
+  ENGINE = innodb;
 
-create table department (
-  id                bigint auto_increment,
-  user_id           bigint       not null,
-  document_logo_id  bigint,
-  name              varchar(255) not null,
-  created_timestamp datetime     not null,
-  updated_timestamp datetime,
-  primary key (id),
-  index (name),
-  index (created_timestamp),
-  index (updated_timestamp),
-  foreign key (user_id) references user (id),
-  foreign key (document_logo_id) references document (id)
+CREATE TABLE department (
+  id                BIGINT AUTO_INCREMENT,
+  user_id           BIGINT       NOT NULL,
+  document_logo_id  BIGINT,
+  name              VARCHAR(255) NOT NULL,
+  created_timestamp DATETIME     NOT NULL,
+  updated_timestamp DATETIME,
+  PRIMARY KEY (id),
+  INDEX (name),
+  INDEX (created_timestamp),
+  INDEX (updated_timestamp),
+  FOREIGN KEY (user_id) REFERENCES user (id),
+  FOREIGN KEY (document_logo_id) REFERENCES document (id)
 )
-  collate = utf8_general_ci
-  engine = innodb;
+  COLLATE = utf8_general_ci
+  ENGINE = innodb;
 
-create table board (
-  id                bigint auto_increment,
-  user_id           bigint       not null,
-  department_id     bigint       not null,
-  name              varchar(255) not null,
-  purpose           text         not null,
-  created_timestamp datetime     not null,
-  updated_timestamp datetime,
-  primary key (id),
-  index (name),
-  index (created_timestamp),
-  index (updated_timestamp),
-  foreign key (user_id) references user (id),
-  foreign key (department_id) references department (id)
+CREATE TABLE board (
+  id                BIGINT AUTO_INCREMENT,
+  user_id           BIGINT       NOT NULL,
+  department_id     BIGINT       NOT NULL,
+  name              VARCHAR(255) NOT NULL,
+  purpose           TEXT         NOT NULL,
+  created_timestamp DATETIME     NOT NULL,
+  updated_timestamp DATETIME,
+  PRIMARY KEY (id),
+  INDEX (name),
+  INDEX (created_timestamp),
+  INDEX (updated_timestamp),
+  FOREIGN KEY (user_id) REFERENCES user (id),
+  FOREIGN KEY (department_id) REFERENCES department (id)
 )
-  collate = utf8_general_ci
-  engine = innodb;
+  COLLATE = utf8_general_ci
+  ENGINE = innodb;
