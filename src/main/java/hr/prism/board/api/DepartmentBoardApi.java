184ff8b3885cc4fe58e1bafa1a68a96ef718b320
fibinger <kuba@fibinger.pl>
2017-03-31 13:09:04 +0200

    API Validation added

--- src/main/java/hr/prism/board/api/DepartmentBoardApi.java
+++ src/main/java/hr/prism/board/api/DepartmentBoardApi.java
@@ -20,83 +20,84 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
 public class DepartmentBoardApi {
-    
+
     @Inject
     private DepartmentService departmentService;
-    
+
     @Inject
     private BoardService boardService;
-    
+
     @Inject
     private BoardMapper boardMapper;
-    
+
     @Inject
     private DepartmentMapper departmentMapper;
-    
+
     @RequestMapping(value = "/departments", method = RequestMethod.GET)
     public List<DepartmentRepresentation> getDepartments() {
         return departmentService.findAllByUserOrderByName().stream().map(departmentMapper.create()).collect(Collectors.toList());
     }
-    
+
     @RequestMapping(value = "/departments/{id}", method = RequestMethod.GET)
     public DepartmentRepresentation getDepartment(@PathVariable("id") Long id) {
         return departmentMapper.create(ImmutableSet.of("boards", "roles")).apply(departmentService.findOne(id));
     }
-    
+
     @RequestMapping(value = "/departments", method = RequestMethod.GET, params = "handle")
     public DepartmentRepresentation getDepartmentByHandle(@RequestParam("handle") String handle) {
         return departmentMapper.create(ImmutableSet.of("boards")).apply(departmentService.findByHandle(handle));
     }
-    
+
     @Restriction(scope = Scope.DEPARTMENT, roles = Role.ADMINISTRATOR)
     @RequestMapping(value = "/departments/{id}", method = RequestMethod.PUT)
-    public void updateDepartment(@PathVariable("id") Long id, @RequestBody DepartmentDTO departmentDTO) {
+    public void updateDepartment(@PathVariable("id") Long id, @RequestBody @Valid DepartmentDTO departmentDTO) {
         departmentService.updateDepartment(id, departmentDTO);
     }
-    
+
     @RequestMapping(value = "/boards", method = RequestMethod.POST)
-    public BoardRepresentation postBoard(@RequestBody BoardDTO boardDTO) {
+    public BoardRepresentation postBoard(@RequestBody @Valid BoardDTO boardDTO) {
         Board board = boardService.createBoard(boardDTO);
         return boardMapper.create(ImmutableSet.of("roles")).apply(board);
     }
-    
+
     @RequestMapping(value = "/boards", method = RequestMethod.GET)
     public List<BoardRepresentation> getBoards() {
         return boardService.findAllByUserOrderByName().stream().map(board -> boardMapper.create().apply(board)).collect(Collectors.toList());
     }
-    
+
     @RequestMapping(value = "/boards/{id}", method = RequestMethod.GET)
     public BoardRepresentation getBoard(@PathVariable("id") Long id) {
         return boardMapper.create(ImmutableSet.of("roles")).apply(boardService.findOne(id));
     }
-    
+
     @RequestMapping(value = "/boards", method = RequestMethod.GET, params = "handle")
     public BoardRepresentation getBoardByHandle(@RequestParam("handle") String handle) {
         return boardMapper.create(ImmutableSet.of("roles")).apply(boardService.findByHandle(handle));
     }
-    
+
     @Restriction(scope = Scope.BOARD, roles = Role.ADMINISTRATOR)
     @RequestMapping(value = "/boards/{id}", method = RequestMethod.PUT)
-    public void updateBoard(@PathVariable("id") Long id, @RequestBody BoardDTO boardDTO) {
+    public void updateBoard(@PathVariable("id") Long id, @RequestBody @Valid BoardDTO boardDTO) {
         boardService.updateBoard(id, boardDTO);
     }
-    
+
     @Restriction(scope = Scope.BOARD, roles = Role.ADMINISTRATOR)
     @RequestMapping(value = "/boards/{id}/settings", method = RequestMethod.PUT)
-    public void updateBoardSettings(@PathVariable("id") Long id, @RequestBody BoardSettingsDTO boardSettingsDTO) {
+    public void updateBoardSettings(@PathVariable("id") Long id, @RequestBody @Valid BoardSettingsDTO boardSettingsDTO) {
         boardService.updateBoardSettings(id, boardSettingsDTO);
     }
-    
+
     @ExceptionHandler(ApiException.class)
     @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
     public Map<String, String> handleException(ApiException apiException) {
         return ImmutableMap.of("exceptionCode", apiException.getExceptionCode().name());
     }
-    
+
 }
