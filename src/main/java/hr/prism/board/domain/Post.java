1548e450f4990be817d3ba3f0785b403763286f0
alastairknowles <alastair@prism.hr>
2017-04-07 07:17:11 +0100

    Mistake merging.

--- src/main/java/hr/prism/board/domain/Post.java
+++ src/main/java/hr/prism/board/domain/Post.java
@@ -19,14 +19,14 @@ import javax.persistence.*;
         name = "board",
         attributeNodes = @NamedAttributeNode("parent")))
 public class Post extends Resource {
-
+    
     @Column(name = "organization_name", nullable = false)
     private String organizationName;
-
+    
     @ManyToOne
     @JoinColumn(name = "location_id", nullable = false)
     private Location location;
-
+    
     @Column(name = "existing_relation")
     @Enumerated(value = EnumType.STRING)
     private RelationWithDepartment existingRelation;
@@ -37,15 +37,15 @@ public class Post extends Resource {
     @URL
     @Column(name = "apply_website")
     private String applyWebsite;
-
+    
     @OneToOne
     @JoinColumn(name = "apply_document_id")
     private Document applyDocument;
-
+    
     @Email
     @Column(name = "apply_email")
     private String applyEmail;
-
+    
     public String getOrganizationName() {
         return organizationName;
     }
@@ -61,12 +61,12 @@ public class Post extends Resource {
     public void setLocation(Location location) {
         this.location = location;
     }
-
+    
     public RelationWithDepartment getExistingRelation() {
         return existingRelation;
     }
     
-    public void setExistingRelation(String existingRelation) {
+    public void setExistingRelation(RelationWithDepartment existingRelation) {
         this.existingRelation = existingRelation;
     }
     
@@ -77,29 +77,29 @@ public class Post extends Resource {
     public void setExistingRelationExplanation(String existingRelationExplanation) {
         this.existingRelationExplanation = existingRelationExplanation;
     }
-
+    
     public String getApplyWebsite() {
         return applyWebsite;
     }
-
+    
     public void setApplyWebsite(String applyWebsite) {
         this.applyWebsite = applyWebsite;
     }
-
+    
     public Document getApplyDocument() {
         return applyDocument;
     }
-
+    
     public void setApplyDocument(Document applyDocument) {
         this.applyDocument = applyDocument;
     }
-
+    
     public String getApplyEmail() {
         return applyEmail;
     }
-
+    
     public void setApplyEmail(String applyEmail) {
         this.applyEmail = applyEmail;
     }
-
+    
 }
