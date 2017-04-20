f1a31cfb05175dac21ff1e5bc694da3be761ab62
alastairknowles <alastair@prism.hr>
2017-04-20 08:41:26 +0100

    Tests passing.

--- src/main/resources/db/migration/V1.18__fix_state_change_npe.sql
+++ src/main/resources/db/migration/V1.18__fix_state_change_npe.sql
@@ -1,7 +1,4 @@
 UPDATE resource
 SET live_timestamp = now(),
-  dead_timestamp   = now() + INTERVAL 1 MONTH;
-
-ALTER TABLE resource
-  MODIFY COLUMN live_timestamp DATETIME NOT NULL,
-  MODIFY COLUMN dead_timestamp DATETIME NOT NULL;
+  dead_timestamp   = now() + INTERVAL 1 MONTH
+WHERE scope = 'POST';
