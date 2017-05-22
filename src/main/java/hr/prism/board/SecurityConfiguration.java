8fcbff4816b599a2e8a02045a2be7c9273ed34db
fibinger <kuba@fibinger.pl>
2017-05-22 11:40:19 +0200

    Spring security made sessionless

--- src/main/java/hr/prism/board/SecurityConfiguration.java
+++ src/main/java/hr/prism/board/SecurityConfiguration.java
@@ -5,23 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    
+
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/auth/*", "/redirect");
     }
-    
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().csrf().disable()
             .anonymous().disable()
             .authorizeRequests()
             .antMatchers("/auth/*", "/redirect").permitAll()
             .and().addFilterAt(new AuthenticationFilter(), BasicAuthenticationFilter.class);
     }
-    
+
 }
