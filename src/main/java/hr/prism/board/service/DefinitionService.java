611af7bbbbd7bb385ab8aefd0369c7dbe3a9f625
alastairknowles <alastair@prism.hr>
2017-05-22 07:24:43 +0100

    Cleaned up definition service

--- src/main/java/hr/prism/board/service/DefinitionService.java
+++ src/main/java/hr/prism/board/service/DefinitionService.java
@@ -1,6 +1,5 @@
 package hr.prism.board.service;
 
-import com.google.common.collect.Sets;
 import hr.prism.board.enums.OauthProvider;
 import org.apache.commons.lang3.text.WordUtils;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -17,43 +16,45 @@ import java.util.stream.Stream;
 
 @Service
 public class DefinitionService {
-
+    
     @SuppressWarnings("unchecked")
-    Set<Class<? extends Enum<?>>> definitionClasses = Sets.newLinkedHashSet();
     private TreeMap<String, Object> definitions;
+    
     @Inject
     private Environment environment;
-
+    
     @PostConstruct
     public TreeMap<String, Object> getDefinitions() {
-        if (definitions == null) {
-            definitions = new TreeMap<>();
-            getDefinitionClasses().forEach(definitionClass -> {
-                List<String> values = Arrays.stream(definitionClass.getEnumConstants()).map(Enum::name).collect(Collectors.toList());
-                definitions.put(getDefinitionKey(definitionClass), values);
-            });
-            definitions.put("applicationUrl", environment.getProperty("app.url"));
-            List<Map<String, Object>> clientIdMap = Stream.of(OauthProvider.values())
-                .map(provider -> {
-                    Map<String, Object> providerMap = new HashMap<>();
-                    providerMap.put("id", provider);
-                    String clientId = environment.getProperty("auth." + provider.name().toLowerCase() + ".clientId");
-                    if (clientId != null) {
-                        providerMap.put("clientId", clientId);
-                    }
-                    return providerMap;
-                })
-                .collect(Collectors.toList());
-            definitions.put("oauthProvider", clientIdMap);
-        }
+        definitions = new TreeMap<>();
+        getDefinitionClasses().forEach(definitionClass -> {
+            List<String> values = Arrays.stream(definitionClass.getEnumConstants()).map(Enum::name).collect(Collectors.toList());
+            definitions.put(getDefinitionKey(definitionClass), values);
+        });
+        
+        definitions.put("applicationUrl", environment.getProperty("app.url"));
+        List<Map<String, Object>> clientIdMap = Stream.of(OauthProvider.values())
+            .map(provider -> {
+                Map<String, Object> providerMap = new HashMap<>();
+                providerMap.put("id", provider);
+                String clientId = environment.getProperty("auth." + provider.name().toLowerCase() + ".clientId");
+                if (clientId != null) {
+                    providerMap.put("clientId", clientId);
+                }
+                
+                return providerMap;
+            })
+            .collect(Collectors.toList());
+        
+        definitions.put("oauthProvider", clientIdMap);
         return definitions;
     }
-
+    
     private Set<Class<? extends Enum<?>>> getDefinitionClasses() {
+        Set<Class<? extends Enum<?>>> definitionClasses = new LinkedHashSet<>();
         try {
             ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
             scanner.addIncludeFilter(new AssignableTypeFilter(Enum.class));
-
+    
             for (BeanDefinition beanDefinition : scanner.findCandidateComponents("hr.prism.board.enums")) {
                 Class<?> clazz = Class.forName(beanDefinition.getBeanClassName());
                 if (Enum.class.isAssignableFrom(clazz)) {
@@ -63,12 +64,12 @@ public class DefinitionService {
         } catch (Exception e) {
             throw new Error(e);
         }
-
+        
         return definitionClasses;
     }
-
+    
     private String getDefinitionKey(Class<? extends Enum<?>> definitionClass) {
         return WordUtils.uncapitalize(definitionClass.getSimpleName());
     }
-
+    
 }
