171faa970373d0ef82505b921847a78c463070c7
fibinger <kuba@fibinger.pl>
2017-05-25 07:34:22 +0200

    Dependency versions upgraded

--- src/main/java/hr/prism/board/service/NotificationService.java
+++ src/main/java/hr/prism/board/service/NotificationService.java
@@ -33,30 +33,30 @@ import java.util.stream.Collectors;
 
 @Service
 public class NotificationService {
-    
+
     private static Logger LOGGER = LoggerFactory.getLogger(NotificationService.class);
-    
+
     private MailStrategy mailStrategy;
-    
+
     private Map<String, String> subjects;
-    
+
     private Map<String, String> contents;
-    
+
     @Inject
     private ResourceService resourceService;
-    
+
     @Inject
     private UserRoleService userRoleService;
-    
+
     @Inject
     private SendGrid sendGrid;
-    
+
     @Inject
     private Environment environment;
-    
+
     @Inject
     private ApplicationContext applicationContext;
-    
+
     @PostConstruct
     public void postConstruct() throws IOException {
         String[] activeProfiles = environment.getActiveProfiles();
@@ -65,15 +65,15 @@ public class NotificationService {
         } else {
             mailStrategy = MailStrategy.SEND;
         }
-        
+
         Resource[] subjects = applicationContext.getResources("classpath:notification/subject/*.html");
         Resource[] contents = applicationContext.getResources("classpath:notification/content/*.html");
-    
+
         this.subjects = indexResources(subjects);
         this.contents = indexResources(contents);
         Assert.isTrue(this.subjects.keySet().equals(this.contents.keySet()), "Every template must have a subject and content");
     }
-    
+
     public Notification makeNotification(String template, User recipient, Map<String, String> customParameters) {
         String recipientEmail = recipient.getEmail();
         String senderEmail = environment.getProperty("system.email");
@@ -82,17 +82,17 @@ public class NotificationService {
         parameters.put("recipient", recipient.getGivenName());
         return new Notification(template, senderEmail, recipientEmail, parameters);
     }
-    
+
     public void sendNotification(Notification notification) {
         String template = notification.getTemplate();
         String sender = notification.getSender();
         String recipient = notification.getRecipient();
         Map<String, String> parameters = notification.getParameters();
-        
+
         StrSubstitutor parser = new StrSubstitutor(parameters);
         String subject = parser.replace(this.subjects.get(template));
         String content = parser.replace(this.contents.get(template));
-        
+
         if (mailStrategy == MailStrategy.LOG) {
             // Local/Test contexts
             LOGGER.info("Sending notification: " + makeLogHeader(template, sender, recipient)
@@ -101,22 +101,22 @@ public class NotificationService {
             // Production/UAT contexts
             Mail mail = new Mail();
             mail.setFrom(new Email(sender));
-            
+
             Personalization personalization = new Personalization();
             personalization.addTo(new Email(recipient));
             mail.addPersonalization(personalization);
-            
+
             mail.setSubject(subject);
             mail.addContent(new Content("text/plain", makePlainTextVersion(content)));
             mail.addContent(new Content("text/html", content));
-            
+
             try {
                 Request request = new Request();
-                request.method = Method.POST;
-                request.endpoint = "mail/send";
-                request.body = mail.build();
+                request.setMethod(Method.POST);
+                request.setEndpoint("mail/send");
+                request.setBody(mail.build());
                 sendGrid.api(request);
-    
+
                 LOGGER.info("Sending notification: " + makeLogHeader(template, sender, recipient));
             } catch (IOException e) {
                 LOGGER.error("Failed to send notification", e);
@@ -124,16 +124,16 @@ public class NotificationService {
             }
         }
     }
-    
+
     private String makeLogHeader(String notification, String sender, String recipient) {
         return "notification=" + notification + ", sender=" + sender + ", recipient=" + recipient;
     }
-    
+
     // FIXME: remove trailing line break
     private String makePlainTextVersion(String html) {
         Document document = Jsoup.parse(html);
         Element body = document.select("body").first();
-        
+
         String plainText = StringUtils.EMPTY;
         for (Element p : body.select("p")) {
             List<Element> as = p.select("a");
@@ -147,57 +147,57 @@ public class NotificationService {
                 plainText = plainText + "\n";
             }
         }
-        
+
         return plainText;
     }
-    
+
     private static Map<String, String> indexResources(Resource[] resources) throws IOException {
         TreeMap<String, String> index = new TreeMap<>();
         for (Resource resource : resources) {
             String fileName = resource.getFilename();
             index.put(fileName.replace(".html", ""), IOUtils.toString(resource.getInputStream(), StandardCharsets.UTF_8).trim());
         }
-        
+
         return index;
     }
-    
+
     private enum MailStrategy {
         LOG, SEND
     }
-    
+
     public static class Notification {
-        
+
         private String template;
-        
+
         private String sender;
-        
+
         private String recipient;
-        
+
         private Map<String, String> parameters;
-        
+
         public Notification(String template, String sender, String recipient, Map<String, String> parameters) {
             this.template = template;
             this.sender = sender;
             this.recipient = recipient;
             this.parameters = parameters;
         }
-        
+
         public String getTemplate() {
             return template;
         }
-        
+
         public String getSender() {
             return sender;
         }
-        
+
         public String getRecipient() {
             return recipient;
         }
-        
+
         public Map<String, String> getParameters() {
             return parameters;
         }
-        
+
     }
-    
+
 }
