bdfdc26689aba3cf831aef95e494775ed29032a2
fibinger <kuba@fibinger.pl>
2017-06-15 14:41:46 +0200

    Missed test fix

--- src/test/java/hr/prism/board/api/ApiExceptionHandlerIT.java
+++ src/test/java/hr/prism/board/api/ApiExceptionHandlerIT.java
@@ -19,36 +19,36 @@ import java.util.Map;
 @TestContext
 @RunWith(SpringRunner.class)
 public class ApiExceptionHandlerIT {
-    
+
     @Inject
     private MockMvc mockMvc;
-    
+
     @Inject
     private ObjectMapper objectMapper;
-    
+
     @Test
     public void shouldHandleGeneralException() throws Exception {
         doRequest("generalException", MockMvcResultMatchers.status().isInternalServerError(), ExceptionCode.PROBLEM);
     }
-    
+
     @Test
     public void shouldHandleApiException() throws Exception {
         doRequest("apiException", MockMvcResultMatchers.status().isUnprocessableEntity(), ExceptionCode.DUPLICATE_DEPARTMENT);
     }
-    
+
     @Test
     public void shouldHandleApiForbiddenException() throws Exception {
-        doRequest("apiForbiddenException", MockMvcResultMatchers.status().isForbidden(), ExceptionCode.UNAUTHENTICATED_USER);
+        doRequest("apiForbiddenException", MockMvcResultMatchers.status().isUnauthorized(), ExceptionCode.UNAUTHENTICATED_USER);
     }
-    
+
     private void doRequest(String path, ResultMatcher resultMatcher, ExceptionCode exceptionCode) throws Exception {
         String response = mockMvc.perform(MockMvcRequestBuilders.get("/test/" + path))
             .andExpect(resultMatcher).andReturn().getResponse().getContentAsString();
-        
+
         Map<String, String> deserializedResponse = objectMapper.readValue(response, new TypeReference<Map<String, String>>() {
         });
-        
+
         Assert.assertEquals(exceptionCode.name(), deserializedResponse.get("exceptionCode"));
     }
-    
+
 }
