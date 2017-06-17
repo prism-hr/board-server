b8b30afc3ce12b9011afaf91572dfebea9c50710
Alastair Knowles <alastairknowles@Alastairs-MacBook-Air.local>
2017-06-17 06:55:44 +0100

    Stored procedure to fix member category data

--- src/main/resources/database/V1.34__remap_member_category.sql
+++ src/main/resources/database/V1.34__remap_member_category.sql
@@ -2,12 +2,12 @@ DELIMITER //
 
 CREATE PROCEDURE remap_resource_category()
   BEGIN
-    CREATE TEMPORARY TABLE department_remap (
+    CREATE TEMPORARY TABLE remap (
       id BIGINT UNSIGNED NOT NULL,
       PRIMARY KEY (id))
     ENGINE = MEMORY;
 
-    INSERT INTO department_remap(id)
+    INSERT INTO remap(id)
       SELECT resource_category.resource_id
       FROM resource_category
       INNER JOIN resource
@@ -19,10 +19,68 @@ CREATE PROCEDURE remap_resource_category()
     DELETE
     FROM resource_category
     WHERE resource_category.resource_id IN(
-      SELECT id
-      FROM department_remap);
+      SELECT remap.id
+      FROM remap)
+      AND resource_category.type = 'MEMBER';
 
-    DROP TABLE department_remap;
+    INSERT INTO resource_category(resource_id, name, type, ordinal, created_timestamp, updated_timestamp)
+      SELECT remap.id, 'UNDERGRADUATE', 'MEMBER', 0, now(), now()
+      FROM remap
+        UNION
+      SELECT remap.id, 'MASTER', 'MEMBER', 1, now(), now()
+      FROM remap
+        UNION
+      SELECT remap.id, 'RESEARCH', 'MEMBER', 2, now(), now()
+      FROM remap;
+
+    DELETE
+    FROM resource_category
+    WHERE resource_id IN(
+      SELECT resource.id
+      FROM resource
+      INNER JOIN resource_relation
+        ON resource.id = resource_relation.resource2_id
+      WHERE resource.scope = 'POST'
+        AND resource_relation.resource1_id IN(
+          SELECT remap.id
+          FROM remap))
+      AND resource_category.type = 'MEMBER';
+
+    INSERT INTO resource_category(resource_id, name, type, ordinal, created_timestamp, updated_timestamp)
+      SELECT resource.id, 'UNDERGRADUATE', 'MEMBER', 0, now(), now()
+      FROM resource
+      INNER JOIN resource_relation
+        ON resource.id = resource_relation.resource2_id
+      WHERE resource.scope = 'POST'
+        AND resource_relation.resource1_id IN(
+          SELECT remap.id
+          FROM remap)
+        UNION
+      SELECT resource.id, 'MASTER', 'MEMBER', 1, now(), now()
+      FROM resource
+        INNER JOIN resource_relation
+          ON resource.id = resource_relation.resource2_id
+      WHERE resource.scope = 'POST'
+            AND resource_relation.resource1_id IN(
+        SELECT remap.id
+        FROM remap);
+
+    DELETE
+    FROM remap;
+
+    INSERT INTO remap(id)
+      SELECT user_role_category.user_role_id
+      FROM user_role_category
+      GROUP BY user_role_category.user_role_id;
+
+    DELETE
+    FROM user_role_category;
+
+    INSERT INTO user_role_category(user_role_id, name, ordinal, created_timestamp, updated_timestamp)
+      SELECT remap.id, 'UNDERGRADUATE', 0, now(), now()
+      FROM remap;
+
+    DROP TABLE remap;
   END //
 
 DELIMITER ;
