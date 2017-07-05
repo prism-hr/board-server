751446da212d5091ab7879abf12ca0043a678fc5
Alastair <alastair@moveguides.com>
2017-07-05 14:11:15 +0300

    Fixed small mistake in query.

--- src/main/java/hr/prism/board/repository/PostRepository.java
+++ src/main/java/hr/prism/board/repository/PostRepository.java
@@ -11,9 +11,9 @@ import java.util.List;
 
 @SuppressWarnings("JpaQlInspection")
 public interface PostRepository extends MyRepository<Post, Long> {
-    
+
     List<Post> findByName(String name);
-    
+
     @Query(value =
         "select post.id " +
             "from Post post " +
@@ -26,7 +26,7 @@ public interface PostRepository extends MyRepository<Post, Long> {
         "select post.id " +
             "from Post post " +
             "where post.state in (:states) " +
-            "and (post.liveTimestamp < :baseline " +
+            "and (post.liveTimestamp <= :baseline " +
             "or post.liveTimestamp is null) " +
             "and (post.deadTimestamp >= :baseline " +
             "or post.deadTimestamp is null)")
