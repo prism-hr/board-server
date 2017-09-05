844596943323092edfff590db4f041a12ef42280
Alastair <alastair@moveguides.com>
2017-09-06 00:26:27 +0200

    Problemsin clean up script fixed.

--- src/main/resources/database/V1.63__squash_user_role.sql
+++ src/main/resources/database/V1.63__squash_user_role.sql
@@ -1,8 +1,15 @@
-CREATE TEMPORARY TABLE max_user_role (
+CREATE TEMPORARY TABLE user_role_retain (
   resource_id BIGINT UNSIGNED NOT NULL,
   user_id     BIGINT UNSIGNED NOT NULL,
   role        VARCHAR(50)     NOT NULL,
   PRIMARY KEY (resource_id, user_id)
+)
+  COLLATE = utf8_general_ci
+  ENGINE = MEMORY;
+
+CREATE TEMPORARY TABLE user_role_delete (
+  id BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (id)
 )
   COLLATE = utf8_general_ci
   ENGINE = MEMORY;
@@ -47,7 +54,7 @@ DELETE
 FROM user_role
 WHERE user_role.state = 'REJECTED';
 
-INSERT INTO max_user_role (resource_id, user_id, role)
+INSERT INTO user_role_retain (resource_id, user_id, role)
   SELECT
     resource_id,
     user_id,
@@ -55,7 +62,7 @@ INSERT INTO max_user_role (resource_id, user_id, role)
   FROM user_role
   WHERE user_role.role = 'ADMINISTRATOR';
 
-INSERT IGNORE INTO max_user_role (resource_id, user_id, role)
+INSERT IGNORE INTO user_role_retain (resource_id, user_id, role)
   SELECT
     resource_id,
     user_id,
@@ -63,7 +70,7 @@ INSERT IGNORE INTO max_user_role (resource_id, user_id, role)
   FROM user_role
   WHERE user_role.role = 'AUTHOR';
 
-INSERT IGNORE INTO max_user_role (resource_id, user_id, role)
+INSERT IGNORE INTO user_role_retain (resource_id, user_id, role)
   SELECT
     resource_id,
     user_id,
@@ -71,19 +78,23 @@ INSERT IGNORE INTO max_user_role (resource_id, user_id, role)
   FROM user_role
   WHERE user_role.role = 'MEMBER';
 
+INSERT INTO user_role_delete
+  SELECT user_role.id
+  FROM user_role
+    LEFT JOIN user_role_retain
+      ON user_role.resource_id = user_role_retain.resource_id
+         AND user_role.user_id = user_role_retain.user_id
+         AND user_role.role = user_role_retain.role
+  WHERE user_role_retain.resource_id IS NULL;
+
 DELETE
 FROM activity_role
 WHERE activity_id IN (
   SELECT id
   FROM activity
   WHERE activity.user_role_id IN (
-    SELECT user_role.id
-    FROM user_role
-      LEFT JOIN max_user_role
-        ON user_role.resource_id = max_user_role.resource_id
-           AND user_role.user_id = max_user_role.user_id
-           AND user_role.role = max_user_role.role
-    WHERE max_user_role.resource_id IS NULL));
+    SELECT id
+    FROM user_role_delete));
 
 DELETE
 FROM activity_event
@@ -91,47 +102,28 @@ WHERE activity_id IN (
   SELECT id
   FROM activity
   WHERE activity.user_role_id IN (
-    SELECT user_role.id
-    FROM user_role
-      LEFT JOIN max_user_role
-        ON user_role.resource_id = max_user_role.resource_id
-           AND user_role.user_id = max_user_role.user_id
-           AND user_role.role = max_user_role.role
-    WHERE max_user_role.resource_id IS NULL));
+    SELECT id
+    FROM user_role_delete));
 
 DELETE
 FROM activity
 WHERE activity.user_role_id IN (
-  SELECT user_role.id
-  FROM user_role
-    LEFT JOIN max_user_role
-      ON user_role.resource_id = max_user_role.resource_id
-         AND user_role.user_id = max_user_role.user_id
-         AND user_role.role = max_user_role.role
-  WHERE max_user_role.resource_id IS NULL);
+  SELECT id
+  FROM user_role_delete);
 
 DELETE
 FROM user_role_category
 WHERE user_role_category.user_role_id IN (
-  SELECT user_role.id
-  FROM user_role
-    LEFT JOIN max_user_role
-      ON user_role.resource_id = max_user_role.resource_id
-         AND user_role.user_id = max_user_role.user_id
-         AND user_role.role = max_user_role.role
-  WHERE max_user_role.resource_id IS NULL);
+  SELECT id
+  FROM user_role_delete);
 
 DELETE
 FROM user_role
 WHERE id IN (
-  SELECT user_role.id
-  FROM user_role
-    LEFT JOIN max_user_role
-      ON user_role.resource_id = max_user_role.resource_id
-         AND user_role.user_id = max_user_role.user_id
-         AND user_role.role = max_user_role.role
-  WHERE max_user_role.resource_id IS NULL);
+  SELECT id
+  FROM user_role_delete);
 
 COMMIT;
 
-DROP TEMPORARY TABLE max_user_role;
+DROP TEMPORARY TABLE user_role_retain;
+DROP TEMPORARY TABLE user_role_delete;
