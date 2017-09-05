30d14c22769d11f6b8f69d5795f2578be2e271ea
Alastair <alastair@moveguides.com>
2017-09-05 10:12:39 +0200

    Fixed bug in search function.

--- src/main/java/hr/prism/board/util/BoardUtils.java
+++ src/main/java/hr/prism/board/util/BoardUtils.java
@@ -1,7 +1,6 @@
 package hr.prism.board.util;
 
 import com.google.common.base.Joiner;
-import com.google.common.collect.Lists;
 import hr.prism.board.dto.ResourcePatchDTO;
 import hr.prism.board.exception.BoardException;
 import hr.prism.board.exception.ExceptionCode;
@@ -29,18 +28,6 @@ public class BoardUtils {
             .build();
 
     private static SimpleTokenizer TOKENIZER = SimpleTokenizer.INSTANCE;
-
-    private static List<String> STOPWORDS = Collections.unmodifiableList(
-        Lists.newArrayList("a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before",
-            "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during",
-            "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers",
-            "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me",
-            "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over",
-            "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them",
-            "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up",
-            "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who",
-            "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves")
-            .stream().map(TOKENIZER::tokenize).flatMap(Arrays::stream).collect(Collectors.toList()));
 
     private static Soundex SOUNDEX = new Soundex();
 
@@ -129,7 +116,7 @@ public class BoardUtils {
             .map(TOKENIZER::tokenize)
             .flatMap(Arrays::stream)
             .filter(string -> string.length() > 1)
-            .filter(string -> !STOPWORDS.contains(string))
+            .map(SOUNDEX::encode)
             .collect(Collectors.joining(" "));
     }
 
