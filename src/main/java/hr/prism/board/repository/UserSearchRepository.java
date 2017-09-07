0ffbe78ced03c3086a073f1ccc93daf4058224a7
fibinger <kuba@fibinger.pl>
2017-09-07 15:51:01 +0200

    Missing JPA Param annotations

--- src/main/java/hr/prism/board/repository/UserSearchRepository.java
+++ src/main/java/hr/prism/board/repository/UserSearchRepository.java
@@ -3,6 +3,7 @@ package hr.prism.board.repository;
 import hr.prism.board.domain.UserSearch;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 
@@ -12,13 +13,13 @@ public interface UserSearchRepository extends MyRepository<UserSearch, Long> {
     @Query(value =
         "INSERT INTO user_search(user_id, search) " +
             "SELECT user.id , :search " +
-            "MATCH user.indexData against(:searchTerm IN BOOLEAN MODE) AS similarity " +
-            "FROM user " +
-            "WHERE user.id IN (:userIds) " +
+            "MATCH USER.indexData against(:searchTerm IN BOOLEAN MODE) AS similarity " +
+            "FROM USER " +
+            "WHERE USER.id IN (:userIds) " +
             "HAVING SIMILARITY > 0 " +
-            "ORDER BY similarity DESC, user.id DESC",
+            "ORDER BY similarity DESC, USER.id DESC",
         nativeQuery = true)
-    void insertBySearch(String search, String searchTerm, Collection<Long> userIds);
+    void insertBySearch(@Param("search") String search, @Param("searchTerm") String searchTerm, @Param("userIds") Collection<Long> userIds);
 
     void deleteBySearch(String search);
 
