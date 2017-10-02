354914a05791debfff7240fb5a2e3be9fa66d20b
alastairknowles <alastair@prism.hr>
2017-10-02 08:10:43 +0100

    More reasonable approach.

--- src/main/java/hr/prism/board/service/PostService.java
+++ src/main/java/hr/prism/board/service/PostService.java
@@ -347,11 +347,14 @@ public class PostService {
         for (ResourceEvent headResourceEvent : headResourceEvents) {
             headResourceEvent.setExposeResponseData(headResourceEvent.getUser().equals(user) ||
                 isAdministrator && BooleanUtils.isTrue(headResourceEvent.getVisibleToAdministrator()));
-            indexByActivities.put(headResourceEvent.getActivity(), headResourceEvent);
+    
+            hr.prism.board.domain.Activity activity = headResourceEvent.getActivity();
+            if (activity != null) {
+                indexByActivities.put(activity, headResourceEvent);
+            }
         }
     
-        for (hr.prism.board.domain.ActivityEvent activityEvent :
-            activityService.findViews(indexByActivities.keySet().stream().filter(Objects::nonNull).collect(Collectors.toList()), user)) {
+        for (hr.prism.board.domain.ActivityEvent activityEvent : activityService.findViews(indexByActivities.keySet(), user)) {
             indexByActivities.get(activityEvent.getActivity()).setViewed(true);
         }
 
