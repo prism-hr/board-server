a763231526e10b9c21d9e8797cede2f022831636
Alastair <alastair@moveguides.com>
2017-11-16 14:19:09 +0100

    Started webhook.

---
+++ src/main/java/hr/prism/board/api/WebhookApi.java
@@ -0,0 +1,11 @@
+package hr.prism.board.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebhookApi {
+
+    @RequestMapping(value = )
+
+}
