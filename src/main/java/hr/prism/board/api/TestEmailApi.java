8fbeaa8ab7334c9e97f175ce0827fc73e3b20e5a
Alastair <alastair@moveguides.com>
2017-11-16 14:19:58 +0100

    Missing controller method mappings.

--- src/main/java/hr/prism/board/api/TestEmailApi.java
+++ src/main/java/hr/prism/board/api/TestEmailApi.java
@@ -4,6 +4,7 @@ import hr.prism.board.representation.TestEmailMessageRepresentation;
 import hr.prism.board.service.TestEmailService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
@@ -16,12 +17,12 @@ public class TestEmailApi {
     @Inject
     private TestEmailService testEmailService;
 
-    @RequestMapping(value = "/api/test/emails")
+    @RequestMapping(value = "/api/test/emails", method = RequestMethod.GET)
     public List<TestEmailMessageRepresentation> getMessages() {
         return testEmailService.findAll();
     }
 
-    @RequestMapping(value = "/api/test/emails/{userId}")
+    @RequestMapping(value = "/api/test/emails/{userId}", method = RequestMethod.GET)
     public List<TestEmailMessageRepresentation> getMessages(@PathVariable Long userId) {
         return testEmailService.findByUserId(userId);
     }
