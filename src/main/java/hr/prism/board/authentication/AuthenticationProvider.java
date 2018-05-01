0bf4dd2b3a45c68d6237ca9477264e122c38647e
alastair <alastair@prism.hr>
2018-05-01 19:04:17 +0100

    Even simpler authentication

--- src/main/java/hr/prism/board/authentication/AuthenticationProvider.java
+++ src/main/java/hr/prism/board/authentication/AuthenticationProvider.java
@@ -4,14 +4,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
-import static org.springframework.security.core.context.SecurityContextHolder.getContext;
-
 @Component
 public class AuthenticationProvider implements org.springframework.security.authentication.AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return getContext().getAuthentication();
+        return authentication;
     }
 
     @Override
