dbe20984648fe2c38c32a5d793347353de731052
Alastair Knowles <ak@mobilize-systems.com>
2018-07-14 07:07:18 +0700

    Started tests for post response

---
+++ src/test/java/hr/prism/board/service/PostResponseServiceIT.java
@@ -0,0 +1,17 @@
+package hr.prism.board.service;
+
+import hr.prism.board.DbTestContext;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@DbTestContext
+@RunWith(SpringRunner.class)
+public class PostResponseServiceIT {
+
+    @Test
+    public void getPostResponse_success() {
+
+    }
+
+}
